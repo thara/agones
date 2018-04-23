2351c426a60ff6151752775f85b9bf6bbeef8af6
Tomochika Hara <tomochika1985@gmail.com>
2018-04-23 23:54:27 +0900

    Refactoring

--- sdks/rust/src/lib.rs
+++ sdks/rust/src/lib.rs
@@ -3,51 +3,48 @@ extern crate grpcio_proto;
 extern crate protobuf;
 extern crate futures;
 
-mod error;
 mod sdk;
 mod sdk_grpc;
 
-use error::SdkError;
 use std::result;
 use std::sync::Arc;
 use std::time::Duration;
 use grpcio::{ChannelBuilder, EnvBuilder};
 
-pub type Result<T> = result::Result<T, error::SdkError>;
-pub type SdkClient = sdk_grpc::SdkClient;
-
 pub trait Sdk {
-    fn ready(&self) -> Result<()>;
-    fn shutdown(&self) -> Result<()>;
-    fn health(&self) -> Result<()>;
+    type Error: std::error::Error;
+    fn ready(&self) -> result::Result<(), Self::Error>;
+    fn shutdown(&self) -> result::Result<(), Self::Error>;
+    fn health(&self) -> result::Result<(), Self::Error>;
 }
 
 const PORT: i32 = 59357;
 
-pub fn new_sdk() -> Result<Box<Sdk>> {
+pub fn new_sdk() -> result::Result<Box<Sdk<Error=grpcio::Error>>, grpcio::Error> {
     let addr = format!("localhost:{}", PORT);
     let env = Arc::new(EnvBuilder::new().build());
     let ch = ChannelBuilder::new(env).keepalive_timeout(Duration::new(30, 0)).connect(&addr);
     let cli = sdk_grpc::SdkClient::new(ch);
     let req = sdk::Empty::new();
-    let _ = cli.ready(&req).map(Box::new).map_err(SdkError::Grpc)?;
+    let _ = cli.ready(&req).map(Box::new)?;
     Ok(Box::new(cli))
 }
 
-impl Sdk for SdkClient {
+impl Sdk for sdk_grpc::SdkClient {
+    type Error = grpcio::Error;
 
-    fn ready(&self) -> Result<()> {
+    fn ready(&self) -> result::Result<(), Self::Error> {
         let req = sdk::Empty::default_instance();
-        self.ready(req).map(|_| ()).map_err(SdkError::Grpc)
+        self.ready(req).map(|_| ())
     }
 
-    fn shutdown(&self) -> Result<()> {
+    fn shutdown(&self) -> result::Result<(), Self::Error> {
         let req = sdk::Empty::default_instance();
-        self.shutdown(req).map(|_| ()).map_err(SdkError::Grpc)
+        self.shutdown(req).map(|_| ())
     }
 
-    fn health(&self) -> Result<()> {
-        self.health().map(|_| ()).map_err(SdkError::Grpc)
+    fn health(&self) -> result::Result<(), Self::Error> {
+        self.health().map(|_| ())
     }
 }
 
@@ -61,7 +58,7 @@ mod tests {
                 let _ = sdk.health();
                 let _ = sdk.shutdown();
             },
-            Err(_) => {
+            Err(_err) => {
             }
         }
     }
