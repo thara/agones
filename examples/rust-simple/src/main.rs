f49f80be7ebccef0d8dd0552271e3e4146ccc29a
Tomochika Hara <tomochika1985@gmail.com>
2018-04-24 23:26:29 +0900

    WIP: Add Rust example

---
+++ examples/rust-simple/src/main.rs
@@ -0,0 +1,47 @@
+extern crate agones;
+extern crate grpcio;
+
+use std::sync::Arc;
+use std::result::Result;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    println!("Rust Game Server has started!");
+    let _ = run();
+}
+
+fn run() -> Result<(), String>{
+
+    println!("Creating SDK instance");
+    let sdk = agones::new_sdk().map_err(|_| "Could not connect to the sidecar. Exiting!")?;
+
+    let sdk = Arc::new(sdk);
+    let t = thread::spawn(|| {
+        loop {
+            if sdk.health().is_err() {
+                println!("Health ping failed");
+            } else {
+                println!("Health ping sent");
+            }
+            thread::sleep(Duration::from_secs(2));
+        }
+    });
+
+    println!("Marking server as ready...");
+
+    for i in 0..10 {
+        let time = i * 10;
+        println!("Running for {} seconds", time);
+
+        thread::sleep(Duration::from_secs(10));
+
+        if i == 5 {
+            println!("Shutting down after 60 seconds...");
+            sdk.shutdown().map_err(|e| format!("Could not run Shutdown(): {}. Exiting!", e));
+            println!("...marked for Shutdown");
+        }
+    }
+
+    Ok(())
+}
