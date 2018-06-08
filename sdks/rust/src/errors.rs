a490a247021b979c3eddc1ce029b3df412f1333c
Tomochika Hara <git@thara.jp>
2018-06-08 22:18:43 +0900

    Fix CrashLoopBackOff in rust-sample

--- sdks/rust/src/errors.rs
+++ sdks/rust/src/errors.rs
@@ -3,4 +3,11 @@ error_chain!{
     foreign_links {
         Grpc(::grpcio::Error);
     }
+
+    errors {
+        HealthPingConnectionFailure(t: String) {
+            description("health ping connection failure"),
+            display("health ping connection failure: '{}'", t),
+        }
+    }
 }
