744ec4ce678cd15bd4706b5c8cb30a8704efcad3
Tomochika Hara <git@thara.jp>
2019-04-02 22:34:43 +0900

    Update rust example

--- examples/rust-simple/src/main.rs
+++ examples/rust-simple/src/main.rs
@@ -46,7 +46,7 @@ fn run() -> Result<(), String> {
     println!("Creating SDK instance");
     let sdk = agones::Sdk::new().map_err(|_| "Could not connect to the sidecar. Exiting!")?;
 
-    let _t = thread::spawn(enclose! {(sdk) move || {
+    let _health = thread::spawn(enclose! {(sdk) move || {
         loop {
             match sdk.health() {
                 (s, Ok(_)) => {
@@ -62,7 +62,34 @@ fn run() -> Result<(), String> {
         }
     }});
 
+    let _watch = thread::spawn(enclose! {(sdk) move || {
+        println!("Starting to watch GameServer updates...");
+        let _ = sdk.watch_gameserver(|gameserver| {
+            println!("GameServer Update, name: {}", gameserver.object_meta.unwrap().name);
+            println!("GameServer Update, state: {}", gameserver.status.unwrap().state);
+        });
+    }});
+
+    println!("Setting a label");
+    sdk.set_label("test-label", "test-value")
+        .map_err(|e| format!("Could not run SetLabel(): {}. Exiting!", e))?;
+
+    println!("Setting an annotation");
+    sdk.set_annotation("test-annotation", "test value")
+        .map_err(|e| format!("Could not run SetAnnotation(): {}. Exiting!", e))?;
+
     println!("Marking server as ready...");
+    sdk.ready()
+        .map_err(|e| format!("Could not run Ready(): {}. Exiting!", e))?;
+
+    println!("...marked Ready");
+
+    println!("Getting GameServer details...");
+    let gameserver = sdk
+        .get_gameserver()
+        .map_err(|e| format!("Could not run GameServer(): {}. Exiting!", e))?;
+
+    println!("GameServer name: {}", gameserver.object_meta.unwrap().name);
 
     for i in 0..10 {
         let time = i * 10;
